36cbfce5b2268af6120ac5f2d88665b4e020d8c2
Kelvin Gikonyo <gikonyosenior@gmail.com>
2024-10-26 17:53:43 +0300

    complete arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+};
+                   
+function destructivelyRemoveLastCat(name) {
+    return cats.pop();
+};
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift();
+};
+
+function appendCat(name) {
+    let newCats = [...cats];
+    newCats.push(name);
+    return newCats;
+};
+function prependCat(name) {
+    let newCats = [...cats];
+    newCats.unshift(name);
+    return newCats;
+};
+
+function removeLastCat() {
+    let newCats = [...cats];
+    newCats.pop();
+    return newCats;
+};
+
+function removeFirstCat() {
+    let newCats = [...cats];
+    newCats.shift();
+    return newCats;
+};
